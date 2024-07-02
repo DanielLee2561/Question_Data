ea2516b0f9250aae5c3ab5554a58111e635f0778
DanielLee2561 <daniel.j.lee.2561@gmail.com>
2024-07-02 16:49:34 -0700

    Tests JSON Fibonacci & Map

--- test/Fibonacci Sequence.js
+++ test/Fibonacci Sequence.js
@@ -37,3 +37,18 @@ describe('Fibonacci Sequence', () => {
 	});
 
 });
+
+function foo_tests()
+{
+  let tests = new Array();
+  tests.push(new test("foo(-2) should equal -1", "chai.expect(foo(-2)).to.eql(-1);"));
+  tests.push(new test("foo(-1) should equal -1", "chai.expect(foo(-1)).to.eql(-1);"));
+  tests.push(new test("foo(0) should equal 0", "chai.expect(foo(0)).to.eql(0);"));
+  tests.push(new test("foo(1) should equal 1", "chai.expect(foo(1)).to.eql(1);"));
+  tests.push(new test("foo(2) should equal 1", "chai.expect(foo(2)).to.eql(1);"));
+  tests.push(new test("foo(3) should equal 2", "chai.expect(foo(3)).to.eql(2);"));
+  tests.push(new test("foo(4) should equal 3", "chai.expect(foo(4)).to.eql(3);"));
+  tests.push(new test("foo(5) should equal 5", "chai.expect(foo(5)).to.eql(5);"));
+  tests.push(new test("foo(6) should equal 8", "chai.expect(foo(6)).to.eql(8);"));
+  return tests;
+}
--- test/Map.js
+++ test/Map.js
@@ -1,31 +1,44 @@
 describe('Map', () => {
 
-	it('foo(1, []) should equal _', () => {
+	it('foo(1, []) should equal []', () => {
 		chai.expect(foo(1, [])).to.eql([]);
 	});
 
-	it('foo(0, [6]) should equal _', () => {
+	it('foo(0, [6]) should equal [6]', () => {
 		chai.expect(foo(0, [6])).to.eql([6]);
 	});
 
-	it('foo(1, [6]) should equal _', () => {
+	it('foo(1, [6]) should equal [7]', () => {
 		chai.expect(foo(1, [6])).to.eql([7]);
 	});
 
-	it('foo(2, [6]) should equal _', () => {
+	it('foo(2, [6]) should equal [8]', () => {
 		chai.expect(foo(2, [6])).to.eql([8]);
 	});
 
-	it('foo(0, [-1, 3, 6]) should equal _', () => {
+	it('foo(0, [-1, 3, 6]) should equal [-1, 3, 6]', () => {
 		chai.expect(foo(0, [-1, 3, 6])).to.eql([-1, 3, 6]);
 	});
 
-	it('foo(1, [-1, 3, 6]) should equal _', () => {
+	it('foo(1, [-1, 3, 6]) should equal [0, 4, 7]', () => {
 		chai.expect(foo(1, [-1, 3, 6])).to.eql([0, 4, 7]);
 	});
 
-	it('foo(2, [-1, 3, 6]) should equal _', () => {
+	it('foo(2, [-1, 3, 6]) should equal [1, 5, 8]', () => {
 		chai.expect(foo(2, [-1, 3, 6])).to.eql([1, 5, 8]);
 	});
 
 });
+
+function foo_tests()
+{
+  let tests = new Array();
+  tests.push(new test("foo(1, []) should equal []", "chai.expect(foo(1, [])).to.eql([]);"));
+  tests.push(new test("foo(0, [6]) should equal [6]", "chai.expect(foo(0, [6])).to.eql([6]);"));
+  tests.push(new test("foo(1, [6]) should equal [7]", "chai.expect(foo(1, [6])).to.eql([7]);"));
+  tests.push(new test("foo(2, [6]) should equal [8]", "chai.expect(foo(2, [6])).to.eql([8]);"));
+  tests.push(new test("foo(0, [-1, 3, 6]) should equal [-1, 3, 6]", "chai.expect(foo(0, [-1, 3, 6])).to.eql([-1, 3, 6]);"));
+  tests.push(new test("foo(1, [-1, 3, 6]) should equal [0, 4, 7]", "chai.expect(foo(1, [-1, 3, 6])).to.eql([0, 4, 7]);"));
+  tests.push(new test("foo(2, [-1, 3, 6]) should equal [1, 5, 8]", "chai.expect(foo(2, [-1, 3, 6])).to.eql([1, 5, 8]);"));
+  return tests;
+}
